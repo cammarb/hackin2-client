cbd3340ea61fec8322e5d9ba6ccb7b4ec92ad335
Camila Martinez <camilamarb98@gmail.com>
2024-02-05 14:24:42 +0000

    Resolve merge changes

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,29 +1,10 @@
-<<<<<<< HEAD
-import { configureStore } from '@reduxjs/toolkit'
-import { apiConnection } from './api/apiConnection'
-import authReducer from '../features/auth/authSlice'
-=======
 import { configureStore } from '@reduxjs/toolkit';
 import { apiConnection } from '@/app/api/apiConnection';
 import authReducer from '@/features/auth/authSlice';
->>>>>>> dev_melvin
 
 export const store = configureStore({
   reducer: {
     [apiConnection.reducerPath]: apiConnection.reducer,
-<<<<<<< HEAD
-    auth: authReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiConnection.middleware),
-  devTools: true,
-})
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
-=======
     auth: authReducer
   },
   middleware: (getDefaultMiddleware) =>
@@ -35,4 +16,3 @@ export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
->>>>>>> dev_melvin
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-<<<<<<< HEAD
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-=======
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()]
->>>>>>> dev_melvin
 });
