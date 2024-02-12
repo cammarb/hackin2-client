ce600100b0c10d4b6a5d8e54e5f85dd743911f1a
Camila Martinez <camilamarb98@gmail.com>
2024-02-12 13:38:39 +0000

    .prettierrc format

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar';
 
 function App() {
   return (
     <>
-      <Header />
+      <Sidebar />
       <Outlet />
     </>
   );
