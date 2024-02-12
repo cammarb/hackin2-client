4e3d7185df2de3b04db68e98cd50b9c74e72886b
Camila Martinez <camilamarb98@gmail.com>
2024-02-12 14:20:25 +0000

    Add Sidebar and SidebarItems component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,40 @@
-export default function Sidebar() {
-  return <></>;
+import { cn } from '@/lib/utils';
+import { NavLink } from 'react-router-dom';
+
+export function SidebarItems({ items }) {
+  return items?.length ? (
+    <div className="grid grid-flow-row auto-rows-max text-sm">
+      {items.map((item, index) =>
+        !item.disabled && item.href ? (
+          <NavLink
+            key={index}
+            to={item.href}
+            // className={cn(
+            //   'flex w-full items-center rounded-md p-2 hover:underline',
+            //   {
+            //     'bg-muted': is === item.href
+            //   }
+            // )}
+            target={item.external ? '_blank' : ''}
+            rel={item.external ? 'noreferrer' : ''}
+          >
+            {item.logo}
+            {item.title}
+          </NavLink>
+        ) : (
+          <span className="flex w-full cursor-not-allowed items-center rounded-md p-2 opacity-60">
+            {item.title}
+          </span>
+        )
+      )}
+    </div>
+  ) : null;
+}
+
+export default function Sidebar({ items }) {
+  return items.length ? (
+    <div className="w-full bg-red">
+      <SidebarItems items={items} />
+    </div>
+  ) : null;
 }
