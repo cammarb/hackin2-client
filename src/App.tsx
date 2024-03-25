6f086e130e006b01b68660dc13188c29e756abf1
Camila Martinez <camilamarb98@gmail.com>
2024-03-25 16:01:16 +0100

    update header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import Header from '@/components/Company/Header'; 
+import { Header } from '@/components/Header';
 
 function App() {
   return (
     <>
-      {/* TEST HEADER */}
-      <header className="p-4 flex gap-8">
-        <Link to={'company/programs'}>Programs</Link>
-        <Link to={'login'}>Login</Link>
-        <Link to={'register'}>Register</Link>
-      </header>
+      <Header />
       <Outlet />
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,9 +10,11 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { ModeToggle } from './mode-toggle';
 import { Navbar } from './Navbar';
-import whiteLogo from '/Hackin2_logo_white.svg'
-import blackLogo from '/Hackin2_logo_black.svg'
+import whiteLogo from '/Hackin2_logo_white.svg';
+import blackLogo from '/Hackin2_logo_black.svg';
 import { useTheme } from './theme-provider';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '@/features/auth/authSlice';
 
 const links = [
   {
@@ -33,34 +35,51 @@ const links = [
   }
 ];
 
+const authLinks = [
+  {
+    title: 'Login',
+    url: 'login'
+  },
+  {
+    title: 'Register',
+    url: 'register'
+  }
+];
+
 export const Header = () => {
+  const user = useSelector(selectCurrentUser);
   const { theme } = useTheme();
 
   const logo = theme === 'light' ? blackLogo : whiteLogo;
 
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="sticky top-0 flex h-16 items-center border-b bg-background px-4 md:px-6">
       <img src={logo} alt="Logo" className="h-8 w-auto" />
-      <Navbar links={links} />
 
-      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+      <Navbar links={links} className="justify-center flex-grow" />
+
+      <div className="flex items-center justify-end gap-4">
         <ModeToggle />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" size="icon" className="rounded-full">
-              <CircleUser className="h-5 w-5" />
-              <span className="sr-only">Toggle user menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        {user ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <Navbar links={authLinks} />
+        )}
       </div>
     </header>
   );
