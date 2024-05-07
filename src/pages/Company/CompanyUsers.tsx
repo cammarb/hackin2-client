93fd4c2e3b72db53ead71598ced81c39c03cf570
Camila Martinez <camilamarb98@gmail.com>
2024-05-07 17:34:56 +0200

    Add users table: - Gets users from API and displays each per row

--- src/pages/Company/CompanyUsers.tsx
+++ src/pages/Company/CompanyUsers.tsx
@@ -1,6 +1,34 @@
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '@/features/auth/authSlice';
 import { useGetCompanyMembersQuery } from '@/features/company/companySlice';
+import {
+  MoreHorizontal,
+} from "lucide-react"
+
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function CompanyUsers() {
   const user = useSelector(selectCurrentUser);
@@ -20,13 +48,61 @@ export default function CompanyUsers() {
     console.log(response.members);
     content = (
       <>
-        <h1>Users</h1>
-        {members.map((member, index) => (
-          <div key={index}>
-            <h1>{member.userId}</h1>
-            <h1>{member.companyRole}</h1>
-          </div>
-        ))}
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+          <Card x-chunk="dashboard-06-chunk-0">
+            <CardHeader>
+              <CardTitle>Users</CardTitle>
+              <CardDescription>
+                Manage the company users and view their role.</CardDescription>
+            </CardHeader>
+
+
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Role</TableHead>
+                    <TableHead>
+                      <span className="sr-only">Actions</span>
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {members.map((member, index) => (
+                    <TableRow key={index}>
+                      <TableCell className="font-medium">
+                        {member.userId}                        </TableCell>
+                      <TableCell>
+                        <Badge variant="outline">{member.companyRole}</Badge>
+                      </TableCell>
+                      <TableCell>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button
+                              aria-haspopup="true"
+                              size="icon"
+                              variant="ghost"
+                            >
+                              <MoreHorizontal className="h-4 w-4" />
+                              <span className="sr-only">Toggle menu</span>
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </main >
       </>
     );
   } else if (isError) {
