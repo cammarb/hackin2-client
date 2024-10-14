547e0c67a3c2d75e82ec7cb19e891de7bbe7a866
Camila Martinez <camilamarb98@gmail.com>
2024-10-14 09:27:09 +0200

    add logout test

--- src/__tests__/login.test.ts
+++ src/__tests__/login.test.ts
@@ -2,6 +2,7 @@ import { vi, expect, test, describe } from 'vitest'
 import createFetchMock from 'vitest-fetch-mock'
 import { setupStore } from '@/app/store'
 import { authApiSlice } from '@/features/auth/authApiSlice'
+import { setCredentials } from '@/features/auth/authSlice'
 
 const fetchMocker = createFetchMock(vi)
 fetchMocker.enableMocks()
@@ -54,5 +55,38 @@ describe('test login mutation', async () => {
 
     expect(state.auth.user).toBe(null)
     expect(state.session.isLoggedIn).toBe(false)
+  })
+})
+
+describe('test logout mutation', async () => {
+  test('something', async () => {
+    const store = setupStore()
+
+    store.dispatch(
+      setCredentials({
+        user: {
+          id: 'testId',
+          username: 'testUsername',
+          role: 'ENTERPRISE',
+          token: 'testToken',
+          company: {
+            id: 'testCompanyId',
+            role: 'OWNER'
+          }
+        }
+      })
+    )
+
+    fetchMocker.mockResponse(
+      JSON.stringify({
+        message: 'success!'
+      })
+    )
+
+    await store.dispatch(authApiSlice.endpoints.logout.initiate({}))
+    const userState = store.getState().auth.user
+    const sessionState = store.getState().session.isLoggedIn
+    expect(userState).toBe(null)
+    expect(sessionState).toBe(false)
   })
 })
